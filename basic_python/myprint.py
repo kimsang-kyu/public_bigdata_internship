3af7993c5b37546fb14b110cc29d2701420c4ad2
kimsangkyu <81627617+kimsang-kyu@users.noreply.github.com>
2021-07-13 16:50:35 +0900

    Add files via upload

---
+++ basic_python/myprint.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def print1(st):
+    print('*'*50)
+    print(st)
+    print('*'*50)
+    
+def print2(st):
+    print('-'*50)
+    print(st)
+    print('-'*50)
+
